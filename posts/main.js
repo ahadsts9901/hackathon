2ce7fa1f071c877984a249e2f060c528fc987080
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-08-19 09:05:15 -0700

    link

--- posts/main.js
+++ posts/main.js
@@ -17,7 +17,7 @@ firebase.auth().onAuthStateChanged(function(user) {
         username = user.email.slice(0, -10); // Store the username
         document.getElementById("headerName").innerText = username;
     } else {
-        window.location.pathname = "/login/index.html";
+        window.location.pathname = "/hackathon/login/index.html";
         document.getElementById("headerName").innerText = 'null';
     }
 });
